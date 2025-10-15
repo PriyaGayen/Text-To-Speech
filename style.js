836b5f543bbbeee0809b9c3b8f5026ad03b2e544
HIMANSHU JAISWAL <priyagayen74@gmail.com>
2025-10-16 04:49:28 +0530

    TEXT TO SPEECH

---
+++ style.js
@@ -0,0 +1,10 @@
+let textArea=document.getElementById("text");
+let copyIcon=document.querySelector(".icon1");
+let delIcon=document.querySelector(".icon2");
+let voiceSelector=document.getElementById("voice-lang");
+let speedSlider=document.getElementById("speed");
+let latchSlider=document.getElementById("latch");
+let volumeSlider=document.getElementById("volume");
+let playButton=document.querySelector("button");
+
+
